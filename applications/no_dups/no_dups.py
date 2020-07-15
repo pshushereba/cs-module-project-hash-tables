a45cd756d5d58b5538a7c822b6cd014bd586d61e
Patrick Shushereba <pshushereba@gmail.com>
2020-07-15 14:08:19 -0700

    Finished no_dups

--- applications/no_dups/no_dups.py
+++ applications/no_dups/no_dups.py
@@ -1,5 +1,16 @@
 def no_dups(s):
-    # Your code here
+  duplicates = {}
+  unique_str = ""
+
+  s = s.split(' ')
+  for word in s:
+    if word in duplicates:
+      continue
+    else:
+      duplicates[word] = word
+      unique_str += word + " "
+  return unique_str.rstrip()
+            
 
 
 
