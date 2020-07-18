cb19ced95a2d6fda6e7533ca3f4850da02938d9f
Patrick Shushereba <pshushereba@gmail.com>
2020-07-17 17:13:15 -0700

    Finished histogram problem.

--- applications/histo/histo.py
+++ applications/histo/histo.py
@@ -1,2 +1,24 @@
 # Your code here
 
+# Read from the file.
+with open("robin.txt") as f:
+    words = f.read()
+
+word_list = words.split(' ')
+# create a hash table
+histogram = {}
+
+# iterate through the word list and make each word the key, and the value a list
+# if the word is in the hash table already, add a hash symbol to the value list.
+
+for word in word_list:
+    if word.lower() not in histogram:
+        histogram[word.lower()] = '#'
+    else:
+        histogram[word.lower()] += '#'
+
+frequency = list(histogram.items())
+frequency.sort(key=lambda x: x[1], reverse=True)
+
+for key, value in frequency:
+    print(f'{key:} {value}')
--- applications/markov/markov.py
+++ applications/markov/markov.py
@@ -6,8 +6,50 @@ with open("input.txt") as f:
 
 # TODO: analyze which words can follow other words
 # Your code here
+word_list = words.replace("\n", ' ').split(" ")
+word_bank = {}
 
+for idx, word in enumerate(word_list):
+    if idx < (len(word_list) -2):
+        if word not in word_bank:
+            word_bank[word] = []
+            word_bank[word].append(word_list[idx + 1])
+        else:
+            word_bank[word].append(word_list[idx + 1])
+
+# pull all of the word_bank keys into a list
+dict_keys = list(word_bank.keys())
+
+def getFirstWord():
+    # choose a random entry as the start word.
+    first_word = random.choice(dict_keys)
+    # check to see if the word is a "start word"
+    if first_word[0].isupper() or first_word[0] == '"' and first_word[0] is not None:
+        return first_word
+    else:
+       return getFirstWord()
+
+def getRandomWord(start_word):
+    rand_word = random.choice(dict_keys[start_word])
+    return rand_word
+
+print(getRandomWord(getFirstWord()))
+#print(dict_keys)
 
 # TODO: construct 5 random sentences
 # Your code here
 
+# Choose a random "start word" to begin.
+# * Print the word.
+# * If it's a "stop word", stop.
+# * Else randomly choose a word that can follow this one.
+
+
+
+# Will start with a capital letter, or a " followed by a capital letter.
+# print the start word.
+# * If it's a "stop word", stop.
+# Else, choose a random word from the list associated with the start word key.
+# store the values of the start word key in a new variable
+# random.choice()
+# 
